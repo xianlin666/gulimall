d1388bdcbd3abc103f3b57c50d6790145ce360f0
xianlin666 <xianlin@zju.edu.cn>
2023-04-17 19:29:39 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/main/java/com/lxl/gulimall/coupon/GulimallCouponApplication.java
@@ -0,0 +1,13 @@
+package com.lxl.gulimall.coupon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallCouponApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallCouponApplication.class, args);
+    }
+
+}
